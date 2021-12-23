00d3e9725205e58bca4aacf139026ee039d953e7
Aleksander M <aleksander.michalski222@gmail.com>
2021-12-23 06:01:17 +0100

    poprawa wcięć

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Wprowadz kurs dolara:");
-    Scanner scanner = new Scanner(System.in);
-    String kurs1 = scanner.next();
-    Double kurs2 = Double.parseDouble(kurs1);
-    scanner.close();
-    Double kurs3 = kurs2 * 100;
-    System.out.println("Zmienna 1:" + kurs2);
-    System.out.println("Zmienna 2:" + kurs3);
+	    System.out.println("Wprowadz kurs dolara:");
+            Scanner scanner = new Scanner(System.in);
+
+            String kurs1 = scanner.next();
+            Double kurs2 = Double.parseDouble(kurs1);
+            scanner.close();
+
+            Double kurs3 = kurs2 * 100;
+
+        System.out.println("Zmienna 1:" + kurs2);
+        System.out.println("Zmienna 2:" + kurs3);
     }
 }
